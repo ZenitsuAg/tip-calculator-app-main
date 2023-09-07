b8e6540b34a75f334cf398a4e36ab4ccf8a90aa5
Fred Robins <fredrobins619@outlook.com>
2023-09-07 14:04:13 +0000

    Update App.jsx and setup gh-pages

--- my-work/src/App.jsx
+++ my-work/src/App.jsx
@@ -1,9 +1,5 @@
-import {Fragment, useState} from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import WhiteSpace from './Components/WhiteSpace'
-import Input from './Components/Input'
 
 function App() {
   // Some code
