54dab82a815f284c4c339816ebb4f1bde09f618e
Fred Robins <fredrobins619@outlook.com>
2025-11-29 12:17:20 +0000

    Update App logic, UI structure & format code

--- my-work/src/Components/Input.jsx
+++ my-work/src/Components/Input.jsx
@@ -1,11 +1,11 @@
 import { useState } from "react";
 
-export default function Input({ src, alt, onChange, step, className }) {
+export default function Input({ src, alt, onChange, step, className, placeholder, value }) {
   // const [value, setValue] = useState("");
 
   return (
-    <div className={className}>
-      <img className="pl-2 object-none" src={src} alt={alt} />
+    <div className={`${className} rounded overflow-hidden border border-very-light-grayish-cyan`}>
+      <img className="pl-4 object-none" src={src} alt={alt} />
       <input
         type="number"
         step={step}
@@ -13,8 +13,8 @@ export default function Input({ src, alt, onChange, step, className }) {
         className="outline-none p-3 bg-very-light-grayish-cyan text-right w-full text-very-dark-cyan text-xl"
         onChange={onChange}
         pattern="/[0-9]+/"
-        
-        // value={value}
+        placeholder={placeholder}
+        value={value}
       />
     </div>
   );
--- my-work/src/Components/WhiteSpace.jsx
+++ my-work/src/Components/WhiteSpace.jsx
@@ -1,23 +1,14 @@
 import {useState} from 'react'
 import Button from './Button'
 import Input from './Input'
-import {CustomButton} from './Button'
 import dollarsign from '../assets/images/icon-dollar.svg'
 import iconPerson from '../assets/images/icon-person.svg'
 import TipResults from './TipResults'
 
 export default function WhiteSpace() {
   const [bill, setBill] = useState(0)
-  const [person, setPerson] = useState(1)
+  const [person, setPerson] = useState(0)
   const [percentage, setPercentage] = useState(0)
-
-  function handleBill(e) {
-    setBill(e.target.value)
-  }
-
-  function HandlePerson(e) {
-    setPerson(e.target.value)
-  }
 
   // Function to update btnValue
   function handlePercentage(btnValue) {
@@ -29,7 +20,7 @@ export default function WhiteSpace() {
   function handleReset() {
     setBill(0)
     setPercentage(0)
-    setPerson(1)
+    setPerson('')
   }
 
   // The logic for the tip calculator
@@ -38,46 +29,79 @@ export default function WhiteSpace() {
   const total = (bill / person + parseFloat(tipAmountPerPerson)).toFixed(2)
 
   return (
-    <div className="bg-white p-10 rounded-t-2xl md:p-8 md:rounded-3xl text-dark-grayish-cyan text-base text-left h-2/3 md:flex md:flex-row md:gap-0 md:min-w-[60%]">
+    <div className="bg-white p-10 rounded-t-2xl md:p-8 md:rounded-3xl text-dark-grayish-cyan text-base text-left h-2/3 md:flex md:flex-row md:gap-0 md:min-w-[70%]">
       <div className="w-full md:pr-9">
         {/* The Bill input */}
-        <h1 className="mt-3">Bill</h1>
+        <h1 className="my-3">Bill</h1>
         <Input
-          className="input"
+          className="input md:mb-8"
           src={dollarsign}
           alt={'Dollar sign'}
-          onChange={handleBill}
+          onChange={(e) => setBill(e.target.value)}
           step={'0.01'}
+          placeholder={0}
+          value={bill ? bill : ''}
         />
 
         {/* The tip percentage buttons */}
         <h1>Select Tip %</h1>
-        <div className="mt-3 grid grid-cols-2 gap-3 md:grid-cols-3 w-full">
-          <Button btnValue={5} className="btn" onClick={handlePercentage} />
-          <Button btnValue={10} className="btn" onClick={handlePercentage} />
-          <Button btnValue={15} className="btn" onClick={handlePercentage} />
-          <Button btnValue={20} className="btn" onClick={handlePercentage} />
-          <Button btnValue={25} className="btn" onClick={handlePercentage} />
-          <CustomButton onClick={handlePercentage} />
+        <div className="mt-3 grid grid-cols-2 gap-3 md:grid-cols-3 w-full text-2xl md:mb-8">
+          <Button
+            btnValue={5}
+            className={`btn ${percentage * 100 == 5 && 'active'}`}
+            onClick={handlePercentage}
+          />
+          <Button
+            btnValue={10}
+            className={`btn ${percentage * 100 == 10 && 'active'}`}
+            onClick={handlePercentage}
+          />
+          <Button
+            btnValue={15}
+            className={`btn ${percentage * 100 == 15 && 'active'}`}
+            onClick={handlePercentage}
+          />
+          <Button
+            btnValue={20}
+            className={`btn ${percentage * 100 == 20 && 'active'}`}
+            onClick={handlePercentage}
+          />
+          <Button
+            btnValue={25}
+            className={`btn ${percentage * 100 == 25 && 'active'}`}
+            onClick={handlePercentage}
+          />
+          <input
+            placeholder="Custom"
+            className="input lg:w-[100px] text-2xl placeholder:text-xl placeholder:text-center placeholder:text-grayish-cyan p-2 text-right"
+            onChange={(e) => setPercentage(e.target.value / 100)}
+          />
         </div>
 
         {/* The Person input with conditional logic */}
         <div className="mt-3 flex items-center justify-between">
-          <h1>Number of People</h1>
-          {person == 0 && <p className="text-red-600">Can't be zero</p>}
+          <h1 className="my-4">Number of People</h1>
+          {person == 0 && total == 'NaN' && person != '' && (
+            <p className="text-red-600">Can't be zero</p>
+          )}
         </div>
         <Input
           className={person == 0 ? 'input focus-within:outline-red-600' : 'input'}
           src={iconPerson}
           alt={'Person icon'}
-          onChange={HandlePerson}
+          onChange={(e) => setPerson(e.target.value)}
           step={'0'}
+          placeholder={'0'}
+          value={person}
         />
       </div>
-      <div className="rounded-lg bg-very-dark-cyan p-4 mt-2 text-very-light-grayish-cyan w-full flex flex-col justify-between">
-        <div>
-          <TipResults moneyAbout={'Tip Amount'} moneyValue={tipAmountPerPerson} />
-          <TipResults moneyAbout={'Total'} moneyValue={total} />
+      <div className="rounded-lg bg-very-dark-cyan p-4 px-6 md:px-10 mt-2 text-very-light-grayish-cyan w-full flex flex-col justify-between">
+        <div className="mt-2">
+          <TipResults
+            moneyAbout={'Tip Amount'}
+            moneyValue={tipAmountPerPerson === 'NaN' ? '0.00' : tipAmountPerPerson}
+          />
+          <TipResults moneyAbout={'Total'} moneyValue={total === 'NaN' ? '0.00' : total} />
         </div>
 
         {/* The Reset button */}
