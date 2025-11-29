b08832f45a78ec4c5fe4951f36c35b62627e613d
Fred Robins <fredrobins619@outlook.com>
2025-11-29 12:13:49 +0000

    Add `main` landmark

--- my-work/src/App.jsx
+++ my-work/src/App.jsx
@@ -4,13 +4,13 @@ import WhiteSpace from './Components/WhiteSpace'
 function App() {
   // Some code
   return (
-    <div className="font-space-mono min-w-screen min-h-screen md:flex md:justify-center md:items-center md:flex-col">
+    <main className="font-space-mono min-w-screen min-h-screen md:flex md:justify-center md:items-center md:flex-col">
       <div className="mb-8">
         <h1 className="tracking-[0.75rem] text-very-dark-cyan font-[700]">SPLI</h1>
         <h1 className="tracking-[0.75rem] text-very-dark-cyan font-[700]">TTER</h1>
       </div>
       <WhiteSpace />
-    </div>
+    </main>
   )
 }
 
